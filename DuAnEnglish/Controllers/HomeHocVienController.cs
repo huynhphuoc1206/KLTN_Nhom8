64e511a46c224862c3a1042a4fa519f0c5f42d00
huynhphuoc1206 <huynhphuoc12062003@gmail.com>
2025-04-03 21:36:33 +0700

    up project

---
+++ DuAnEnglish/Controllers/HomeHocVienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuAnEnglish.Controllers
+{
+    public class HomeHocVienController : Controller
+    {
+        // GET: HomeHocVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
