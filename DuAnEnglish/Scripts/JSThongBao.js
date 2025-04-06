305da32f75bbd25f9b77caa94893a4dc016ad6d9
thanh2103 <thanh1thuy2003@gmail.com>
2025-04-06 17:40:16 +0700

    code jsthongbao

---
+++ DuAnEnglish/Scripts/JSThongBao.js
@@ -0,0 +1,27 @@
+// Hàm hi?n th? thông báo SweetAlert2
+function showAlert(message) {
+    Swal.fire({
+        icon: 'error',
+        title: 'L?i!',
+        text: message,
+        background: '#ecf0f1',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK',
+        showCancelButton: false,
+        timer: 5000,
+        customClass: {
+            popup: 'swal2-popup',
+            title: 'swal2-title',
+            content: 'swal2-content',
+            confirmButton: 'swal2-confirm',
+            cancelButton: 'swal2-cancel'
+        },
+        didOpen: () => {
+            document.body.style.overflow = 'hidden'; // Ng?n cu?n khi popup m?
+        },
+        willClose: () => {
+            document.body.style.overflow = ''; // Khôi ph?c cu?n khi popup ?óng
+        }
+    });
+}
