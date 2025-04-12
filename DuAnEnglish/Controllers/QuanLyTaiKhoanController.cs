7c1b859803b2f4f803ead31750126bf3c8315d1d
thanh2103 <thanh1thuy2003@gmail.com>
2025-04-12 17:13:17 +0700

    fe layout admin home

---
+++ DuAnEnglish/Controllers/QuanLyTaiKhoanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuAnEnglish.Controllers
+{
+    public class QuanLyTaiKhoanController : Controller
+    {
+        // GET: QuanLyTaiKhoan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
