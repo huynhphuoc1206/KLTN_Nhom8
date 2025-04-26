e8ce8fd9a69d64abba9031870af76012669f6dc4
huynhphuoc1206 <huynhphuoc12062003@gmail.com>
2025-04-27 00:51:34 +0700

    Add js thong bao thanh cong

--- DuAnEnglish/Scripts/JSThongBao.js
+++ DuAnEnglish/Scripts/JSThongBao.js
@@ -25,3 +25,30 @@ function showAlert(message) {
         }
     });
 }
+// Hàm hiển thị thông báo SweetAlert2
+function showSuccess(message) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Thành công',
+        text: message,
+        background: '#ecf0f1',
+        confirmButtonColor: '#28a745',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK',
+        showCancelButton: false,
+        timer: 5000,
+        customClass: {
+            popup: 'swal2-popup',
+            title: 'swal2-title',
+            content: 'swal2-content',
+            confirmButton: 'swal2-confirm',
+            cancelButton: 'swal2-cancel'
+        },
+        didOpen: () => {
+            document.body.style.overflow = 'hidden'; // Ngăn cuộn khi popup mở
+        },
+        willClose: () => {
+            document.body.style.overflow = ''; // Khôi phục cuộn khi popup đóng
+        }
+    });
+}
